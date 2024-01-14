3b5058b11bc5d2ebb749b93567370befa1aef90a
ZFAbox <starbreeze@yandex.ru>
2024-01-15 00:33:57 +0300

    All changes

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -10,6 +10,59 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     
+    @IBOutlet var imageCellView: UIImageView!{
+        didSet{
+//            imageCellView.layer.cornerRadius = 16
+//            imageCellView.clipsToBounds = true
+//            imageCellView.bounds = bounds
+//            print(imageCellView.bounds.width)
+//            let gradientLayer = CAGradientLayer()
+//            guard let imageHeight = imageCellView.image?.cgImage?.height else {return}
+//            print(imageHeight)
+//            gradientLayer.colors = [UIColor.transperant.cgColor, UIColor.transperantBlue.cgColor]
+//////            gradientLayer.locations = [0.0, 1.0]
+//////            gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+//////            gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
+////
+//            gradientLayer.frame = bounds
+//            gradientLayer.bounds.size.height = 30
+//            gradientLayer.frame.size.width = imageCellView.bounds.width
+//            gradientLayer.position.y = CGFloat(imageCellView.frame.height - 15)
+//
+//            let width = imageCellView.frame.width
+//            print(width)
+//            imageCellView.layer.addSublayer(gradientLayer)
+
+        }
+    }
+    
+    @IBOutlet weak var imageCellViewDate: UILabel!
+    
+    @IBOutlet var gradienCellView: UIView! {
+        didSet{
+            gradienCellView.backgroundColor = UIColor.clear
+            
+//            let gradientLayer = CAGradientLayer()
+//            gradientLayer.colors = [UIColor.transperant.cgColor, UIColor.black.cgColor]
+//            gradientLayer.locations = [0.0, 1.0]
+//            gradientLayer.frame = CGRect(x: 0, y: 0, width: Int(self.gradienCellView.frame.size.width), height: 30)
+////            gradientLayer.frame.size.height = 30
+//            gradienCellView.layer.addSublayer(gradientLayer)
+//            
+        }
+    }
+    
+  
+    
+    
+    @IBOutlet weak var likeCellViewButton: UIButton! {
+        didSet{
+            likeCellViewButton.titleLabel?.text = ""
+        }
+    }
+    
+    let color = Colors()
+    
     static let reuseIdentifier = "ImagesListCell"
     
 }
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -14,46 +14,89 @@ final class ImagesListViewController: UIViewController {
             imagesListTableView.backgroundColor = .ypblack
             imagesListTableView.dataSource = self
             imagesListTableView.delegate = self
-            imagesListTableView.register(ImagesListCell.self, forCellReuseIdentifier: "ImagesListCell")
         }
     }
+    
+    private let photoArray: [String] = Array(0..<20).map{ String($0) }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        // Do any additional setup after loading the view.
+        imagesListTableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0  )
     }
     
-    func configCell(for: ImagesListCell) {
+    func dateFormat(date: Date) -> String {
+        var curentDate = ""
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        dateFormatter.setLocalizedDateFormatFromTemplate("dMMMMyyyy")
+        for charackter in dateFormatter.string(from: date) {
+            if charackter != "г" {
+                if charackter != "." {
+                    curentDate.append(charackter)
+                }
+            }
+        }
+        return curentDate
+    }
+    
+    func configCell(for imagesListCell: ImagesListCell, indexPath: IndexPath) {
+        imagesListCell.imageCellView?.image = UIImage(named: "\(indexPath.row)")
+        print(indexPath.row)
+        imagesListCell.likeCellViewButton.imageView?.tintColor = indexPath.row % 2 == 0 ? UIColor.transperantWhite : UIColor.ypred
+        imagesListCell.imageCellViewDate?.text = dateFormat(date: Date())
         
+        
+        //gradient
+        
+        let gradientLayer = CAGradientLayer()
+        let imageHeight = imagesListCell.frame.height
+        gradientLayer.colors = [
+            UIColor(red: 0.102, green: 0.106, blue: 0.133, alpha: 0).cgColor,
+            UIColor(red: 0.102, green: 0.106, blue: 0.133, alpha: 1).cgColor
+            ]
+        gradientLayer.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: 0, b: 0.54, c: -0.54, d: 0, tx: 0.77, ty: 0))
+        gradientLayer.frame.size.width = imagesListCell.imageCellView.frame.width
+        gradientLayer.bounds = view.bounds.insetBy(dx: -0.5*view.bounds.size.width, dy: -0.5*view.bounds.size.height)
+        gradientLayer.frame.size.height = 30
+        
+        gradientLayer.position.y = CGFloat(imageHeight - 15)
+        gradientLayer.position = imagesListCell.imageCellView.center
+        imagesListCell.imageCellView.layer.addSublayer(gradientLayer)
+                                
     }
-    
 }
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photoArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = imagesListTableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
         
-        guard let imgesListCell = cell as? ImagesListCell else {
-            print("Нет ячейки соответствющей индентификатору ImageListCell")
+        guard let imagesListCell = cell as? ImagesListCell else {
+            print("Ошибка приведения ячейки")
             return UITableViewCell()
         }
         
-        configCell(for: imgesListCell)
-        return imgesListCell
+        configCell(for: imagesListCell, indexPath: indexPath)
+        return imagesListCell
     }
-    
-    
 }
 
 extension ImagesListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+       
+        guard let imageWidth = UIImage(named: "\(indexPath.row)")?.cgImage?.width else {return 10}
+        let cellRowWidth = tableView.frame.width
+        guard let imageHeight = UIImage(named: "\(indexPath.row)")?.cgImage?.height else { return 10 }
+        let cellRowHeight = Int(cellRowWidth) / imageWidth * imageHeight + 8
+        return CGFloat(cellRowHeight)
     }
 }
 
