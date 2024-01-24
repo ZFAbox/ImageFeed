7f2245b4e4ce35abf6bd55ac61f9140b214de4a9
ZFAbox <starbreeze@yandex.ru>
2024-01-25 02:13:34 +0300

    updated Navigation Controller

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Федор Завьялов on 21.01.2024.
+//
+
+import Foundation
---
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Федор Завьялов on 25.01.2024.
+//
+
+import Foundation
