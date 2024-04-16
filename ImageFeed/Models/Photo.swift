0b4f307e6753a4206189c29a52694a140097896a
ZFAbox <starbreeze@yandex.ru>
2024-04-16 08:17:32 -0700

    Обновлена кнопка перехода на WebViewViewController  и добавлены Структуры.

---
+++ ImageFeed/Models/Photo.swift
@@ -0,0 +1,18 @@
+//
+//  Photo.swift
+//  ImageFeed
+//
+//  Created by Fedor on 16.04.2024.
+//
+
+import Foundation
+
+struct Photo {
+    let id: String
+    let size: CGSize
+    let createdAt: Date?
+    let welcomeDescription: String?
+    let thumbImageURL: String
+    let largeImageURL: String
+    let isLiked: Bool
+} 
---
+++ ImageFeed/Models/PhotoResult.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoResult.swift
+//  ImageFeed
+//
+//  Created by Fedor on 16.04.2024.
+//
+
+import Foundation
+
+struct PhotoResult {
+    
+    let id: String
+    let createdAt: String
+    
+    
+    struct UrlsResult {
+        
+    }
+}
