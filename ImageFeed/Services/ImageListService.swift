dcf42bf208ca60ac6c6a81701629b67aee3f8c29
ZFAbox <starbreeze@yandex.ru>
2024-04-18 01:27:43 +0300

    Добавлена функция fetchPhotosNextPage()

--- ImageFeed/Services/ImageListService.swift
+++ ImageFeed/Services/ImageListService.swift
@@ -11,33 +11,25 @@ class ImageListService {
     
     var photos: [Photo] = []
     static let didChangeNotification = Notification.Name(rawValue: "ImageListServiceDidChange")
-    var page: Int?
-    var photosPerPage: Int?
+    var page: Int = 0
+    var perPage: Int = 10
     var task: URLSessionTask?
     
     private enum GetPhotoListError: Error {
         case invalidPhotoRequest
     }
     
-    func makePhotoRequest(token: String, username: String, page: Int?) -> URLRequest? {
-        
-        guard var urlComponents = URLComponents(string: Constants.defaultBaseApiUrl) else {
+    func makePhotoRequest(token: String, username: String, page: Int, perPage: Int) -> URLRequest? {
+        let urlString = Constants.defaultBaseApiUrl + "/photos"
+        guard var urlComponents = URLComponents(string: urlString) else {
             preconditionFailure("ошибка формирования строки запроса авторизации")
         }
-            urlComponents.queryItems = [
-                URLQueryItem(name: URLQueryItemsList.page.rawValue, value: Constants.accessKey),
-                URLQueryItem(name: URLQueryItemsList.clientSecret.rawValue, value: Constants.secretKey),
-                URLQueryItem(name: URLQueryItemsList.redirectURI.rawValue, value: Constants.redirectURI),
-                URLQueryItem(name: URLQueryItemsList.code.rawValue, value: code),
-                URLQueryItem(name: URLQueryItemsList.grantType.rawValue, value: Constants.grantType)
-            ]
-            guard let url = urlComponents.url else {
-                preconditionFailure("Невозможно сформировать ссылку на запрос авторизации")}
-            var request = URLRequest(url: url)
-        
-        guard let url = URL(string: mainUrlProfile) else {
-            preconditionFailure("Ошибка формирования URL")
-        }
+        urlComponents.queryItems = [
+            URLQueryItem(name: URLQueryItemsList.page.rawValue, value: String(page)),
+            URLQueryItem(name: URLQueryItemsList.perPage.rawValue, value: String(perPage))
+        ]
+        guard let url = urlComponents.url else {
+            preconditionFailure("Невозможно сформировать ссылку на запрос авторизации")}
         var request = URLRequest(url: url)
         request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         request.httpMethod = "GET"
@@ -45,14 +37,16 @@ class ImageListService {
     }
     
     func fetchPhotoNextPage (token: String, username: String, handler: @escaping (Result<PhotoResult,Error>) -> Void) {
-        task?.cancel()
-        guard let request = self.makePhotoRequest(token: token, username: username) else {
+        if photos.count < page * perPage || task != nil {task?.cancel()}
+        page += 1
+        guard let request = self.makePhotoRequest(token: token, username: username, page: page, perPage: perPage) else {
             handler(.failure(GetPhotoListError.invalidPhotoRequest))
             return
         }
         let task = URLSession.shared.objectTask(for: request) { (result: Result<PhotoResult, Error>) in
             switch result {
             case .success(let decodedPhotoList):
+                photos = decodedPhotoList
                 handler(.success(decodedPhotoList))
             case .failure(let error):
                 handler(.failure(error))
@@ -60,6 +54,18 @@ class ImageListService {
         }
         self.task = task
         task.resume()
+    }
+    
+    func preparePhotoModel(photoResult: PhotoResult) -> [Photo] {
+        var photoModel: [Photo] = []
+        var photo: Photo
+        for result in photoResult.photos {
+            photo.id = result.id
+            photo.createdAt = result.createdAt
+            photo.size.width = CGFloat(result.width)
+            photo.size.height = CGFloat(result.height)
+            
+        }
     }
     
 }
